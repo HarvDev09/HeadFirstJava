802d1556dd103d6e00e930657a8f7d2b1a19f28c
HarvDev <perezharvey578@gmail.com>
2026-06-29 16:45:14 -0400

    Al ejercicio a habia que agregarle la suma a la variable x, al b habia que agregarle la clase y al c el metodo main

---
+++ src/HarDev/Book/java/ExerciseA.java
@@ -0,0 +1,21 @@
+package HarDev.Book.java;
+
+public class ExerciseA {
+
+    public static void main (String[] args){
+
+    int x = 1;
+while (x < 10)
+
+    {
+
+        x = x + 1;
+        if (x > 3) {
+            System.out.println("big x");
+        }
+
+    }
+
+    }
+
+}
